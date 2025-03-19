00159e8ef40e0f045b813c66a8fe72eb275cef06
Harmony Tayler <153692183+harmonytayler@users.noreply.github.com>
2025-03-19 13:23:20 -0600

    Initial commit

---
+++ backend/WaterProject.API/Controllers/WaterController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WaterProject.API.Data;
+
+namespace WaterProject.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class WaterController : ControllerBase
+    {
+        private WaterDbContext _waterContext;
+        public WaterController(WaterDbContext temp) => _waterContext = temp;
+
+        [HttpGet("AllProjects")]
+        public IEnumerable<Project> GetProjects()
+        {
+            var projectList = _waterContext.Projects.ToList();
+            return projectList;
+        }
+        [HttpGet("FunctionalProjects")]
+        public IEnumerable<Project> GetFunctionalProjects()
+        {
+            var functionalList = _waterContext.Projects.Where(p => p.ProjectFunctionalityStatus == "Functional").ToList();
+            return functionalList;
+        }
+    }
+}
---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import ProjectList from './ProjectList.tsx'
+
+function App() {
+
+  return (
+    <>
+      <ProjectList/>
+    </>
+  )
+}
+
+export default App
