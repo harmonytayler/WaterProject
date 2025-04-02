b8b542c30cec5736236367fbf35fe53e1a9323e7
Harmony Tayler <153692183+harmonytayler@users.noreply.github.com>
2025-04-01 19:31:07 -0600

    Adjusted GetProjectTypes url

--- frontend/src/components/CategoryFilter.tsx
+++ frontend/src/components/CategoryFilter.tsx
@@ -14,7 +14,7 @@ function CategoryFilter ({
     useEffect(() => {
         const fetchCategories = async () => {
             try {
-            const response = await fetch(`https://localhost:5000/Water/GetProjectTypes`);
+            const response = await fetch(`https://waterproject-htayler-backend.azurewebsites.net/Water/GetProjectTypes`);
             const data = await response.json();
             setCategories(data);
             }
